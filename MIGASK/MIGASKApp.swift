f374641b90e1b34753a23b86cd4be264eba82f3f
Doughy <Doughyvibe@gmail.com>
2025-11-16 03:22:19 +0800

    Initial Commit

---
+++ MIGASK/MIGASKApp.swift
@@ -0,0 +1,17 @@
+//
+//  MIGASKApp.swift
+//  MIGASK
+//
+//  Created by Doughy on 16/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MIGASKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
